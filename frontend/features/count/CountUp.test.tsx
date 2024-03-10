97066128ccb1948ed8821ed9eb994681a2672138
sui <1202ayasnppk@gmail.com>
2024-03-10 21:25:40 +0900

    add: 設定ファイル

--- frontend/features/count/CountUp.test.tsx
+++
@@ -1,9 +0,0 @@
-import CountUp from './CountUp';
-import { render } from '@testing-library/react';
-
-describe('CountUp コンポーネントのテスト', () => {
-  test('初期値が0であること', () => {
-    const { getByText } = render(<CountUp />);
-    expect(getByText('現在のカウント: 0')).toBeInTheDocument();
-  });
-});
